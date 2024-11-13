73fc548e83103a394e088101d4f70eaa7d342fc5
kbgozon <152581301+kbgozon@users.noreply.github.com>
2024-11-13 00:26:21 +0000

    Create new doc type

---
+++ my_app01/my_app01_test/doctype/kirk_doc_type/kirk_doc_type.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, kirk clarenz gozon and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class kirkdoctype(Document):
+	pass
